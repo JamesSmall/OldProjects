12ef4d4d61cc834407f553edc986ba1a9935d7ac
JamesSmall <32995775+JamesSmall@users.noreply.github.com>
2018-06-24 22:36:25 -0500

    Add files via upload

---
+++ GameEngineIteration2/Compiled/Resources/Load.js
@@ -0,0 +1,7 @@
+Load.loadBasicTexture("NakedHead","Resources/Texture/Gear/NoGear/Head.png");
+Load.loadBasicTexture("NakedChest","Resources/Texture/Gear/NoGear/Chest.png");
+Load.loadBasicTexture("NakedHand","Resources/Texture/Gear/NoGear/Hand.png");
+Load.loadBasicTexture("Target","Resources/Texture/Sample/Target.png");
+Load.loadBasicTexture("PigHead","Resources/Texture/Animal/Pig/Head.png");
+Load.loadBasicTexture("PigBody","Resources/Texture/Animal/Pig/Chest.png");
+Load.createPrimaryGlyph("test","Resources/Texture/Text/Text.png","Resources/Texture/Text/TextReader.GRF");
